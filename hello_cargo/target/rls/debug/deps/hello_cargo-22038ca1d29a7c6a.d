9c0e1b660d85c4d58858c92511be7789ce77d94d
wjuid <wjuid@163.com>
2021-01-16 21:54:40 +0800

    frist1

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-22038ca1d29a7c6a.d
@@ -0,0 +1,5 @@
+/home/wju/My_Projects/Rust_Projects/hello_cargo/target/rls/debug/deps/hello_cargo-22038ca1d29a7c6a.rmeta: src/main.rs
+
+/home/wju/My_Projects/Rust_Projects/hello_cargo/target/rls/debug/deps/hello_cargo-22038ca1d29a7c6a.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-311fd28801e40214.d
@@ -0,0 +1,5 @@
+/home/wju/My_Projects/Rust_Projects/hello_cargo/target/rls/debug/deps/hello_cargo-311fd28801e40214.rmeta: src/main.rs
+
+/home/wju/My_Projects/Rust_Projects/hello_cargo/target/rls/debug/deps/hello_cargo-311fd28801e40214.d: src/main.rs
+
+src/main.rs:
